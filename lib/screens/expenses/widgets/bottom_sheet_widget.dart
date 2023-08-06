9499a75bf96e96104ad03e951e864893f4bc0b3d
Eleana <eleana@JOD0267L-2.local>
2023-08-06 12:21:29 +0300

    bottom sheet when no cats

--- lib/screens/expenses/widgets/bottom_sheet_widget.dart
+++ lib/screens/expenses/widgets/bottom_sheet_widget.dart
@@ -60,150 +60,151 @@ class _BottomSheetWidgetState extends State<BottomSheetWidget> {
     filteredCategoryList.remove('All');
 
     return Padding(
-      padding: EdgeInsets.only(
-          right: 20,
-          left: 20,
-          bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50, bottom: 40),
-              child: Column(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text(
-                          'Cancel',
-                          style: TextStyle(color: Colors.teal),
-                        )),
-                    Text(
-                      widget.trans == null ? 'Add' : 'Edit',
-                      style: const TextStyle(
-                          color: Colors.teal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                    TextButton(
-                        child: const Text(
-                          'Done',
-                          style: TextStyle(color: Colors.teal),
-                        ),
-                        onPressed: () {
-                          if (!formKey.currentState!.validate()) {
-                            return;
-                          } else {
-                            final newTransaction = Transactions(
-                              desc: descController.text,
-                              price: double.parse(priceController.text),
-                              type: type,
-                              category: selectedCategory ??
-                                  filteredCategoryList.first,
-                            );
-                            if (widget.trans != null) {
-                              newTransaction.uniqueId = widget.trans!.uniqueId;
-                            }
-                            widget.onClicked(newTransaction);
-                            print(
-                                '${newTransaction.uniqueId}===== newtransaction');
-                          }
+        padding: EdgeInsets.only(
+            right: 20,
+            left: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            filteredCategoryList.isNotEmpty
+                ? Form(
+                    key: formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 50, bottom: 40),
+                      child: Column(children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text(
+                                  'Cancel',
+                                  style: TextStyle(color: Colors.teal),
+                                )),
+                            Text(
+                              widget.trans == null ? 'Add' : 'Edit',
+                              style: const TextStyle(
+                                  color: Colors.teal,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                            TextButton(
+                                child: const Text(
+                                  'Done',
+                                  style: TextStyle(color: Colors.teal),
+                                ),
+                                onPressed: () {
+                                  if (!formKey.currentState!.validate()) {
+                                    return;
+                                  } else {
+                                    final newTransaction = Transactions(
+                                      desc: descController.text,
+                                      price: double.parse(priceController.text),
+                                      type: type,
+                                      category: selectedCategory ??
+                                          filteredCategoryList.first,
+                                    );
+                                    if (widget.trans != null) {
+                                      newTransaction.uniqueId =
+                                          widget.trans!.uniqueId;
+                                    }
+                                    widget.onClicked(newTransaction);
+                                  }
 
-                          Navigator.pop(context);
-                          setState(() {});
-                        }),
-                  ],
-                ),
-                const Divider(),
-                filteredCategoryList.isNotEmpty
-                    ? DropdownButton(
-                        value: selectedCategory != null
-                            ? filteredCategoryList.firstWhere(
-                                (element) => element == selectedCategory)
-                            : filteredCategoryList.first,
-                        items: filteredCategoryList.map((category) {
-                          return DropdownMenuItem(
-                            value: category,
-                            child: Text(category),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {
-                          setState(() {
-                            selectedCategory = newValue!;
-                          });
-                        },
-                      )
-                    : Text('Add categories from settings please!'),
-                TextFormField(
-                  controller: priceController,
-                  validator: (value) {
-                    if ((value?.isEmpty ?? true) ||
-                        double.parse(value!) <= 0.0) {
-                      return 'Please add price as number';
-                    } else {
-                      return null;
-                    }
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                      labelText: 'Price..',
-                      prefixIcon: Icon(Icons.price_check),
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                TextFormField(
-                  textInputAction: TextInputAction.newline,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 5,
-                  validator: (value) {
-                    if (value!.isNotEmpty && value.length > 2) {
-                      return null;
-                    } else {
-                      return 'Please add description';
-                    }
-                  },
-                  decoration: const InputDecoration(
-                      labelText: 'Descriprtion..',
-                      prefixIcon: Icon(Icons.money),
-                      border: OutlineInputBorder()),
-                  controller: descController,
-                ),
-                RadioListTile(
-                  activeColor: Colors.teal,
-                  title: const Text('Income'),
-                  value: true,
-                  groupValue: isIncome,
-                  onChanged: (context) {
-                    setState(() {
-                      isIncome = true;
-                      type = TransactionType.income;
-                    });
-                  },
-                ),
-                RadioListTile(
-                  activeColor: Colors.teal,
-                  title: const Text('Outcome'),
-                  value: false,
-                  groupValue: isIncome,
-                  onChanged: (context) {
-                    setState(() {
-                      isIncome = false;
-                      type = TransactionType.outcome;
-                    });
-                  },
-                ),
-              ]),
-            ),
-          )
-        ],
-      ),
-    );
+                                  Navigator.pop(context);
+                                }),
+                          ],
+                        ),
+                        const Divider(),
+                        DropdownButton(
+                          value: selectedCategory != null
+                              ? filteredCategoryList.firstWhere(
+                                  (element) => element == selectedCategory)
+                              : filteredCategoryList.first,
+                          items: filteredCategoryList.map((category) {
+                            return DropdownMenuItem(
+                              value: category,
+                              child: Text(category),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              selectedCategory = newValue!;
+                            });
+                          },
+                        ),
+                        TextFormField(
+                          controller: priceController,
+                          validator: (value) {
+                            if ((value?.isEmpty ?? true) ||
+                                double.parse(value!) <= 0.0) {
+                              return 'Please add price as number';
+                            } else {
+                              return null;
+                            }
+                          },
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                              labelText: 'Price..',
+                              prefixIcon: Icon(Icons.price_check),
+                              border: OutlineInputBorder()),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        TextFormField(
+                          textInputAction: TextInputAction.newline,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 5,
+                          validator: (value) {
+                            if (value!.isNotEmpty && value.length > 2) {
+                              return null;
+                            } else {
+                              return 'Please add description';
+                            }
+                          },
+                          decoration: const InputDecoration(
+                              labelText: 'Descriprtion..',
+                              prefixIcon: Icon(Icons.money),
+                              border: OutlineInputBorder()),
+                          controller: descController,
+                        ),
+                        RadioListTile(
+                          activeColor: Colors.teal,
+                          title: const Text('Income'),
+                          value: true,
+                          groupValue: isIncome,
+                          onChanged: (context) {
+                            setState(() {
+                              isIncome = true;
+                              type = TransactionType.income;
+                            });
+                          },
+                        ),
+                        RadioListTile(
+                          activeColor: Colors.teal,
+                          title: const Text('Outcome'),
+                          value: false,
+                          groupValue: isIncome,
+                          onChanged: (context) {
+                            setState(() {
+                              isIncome = false;
+                              type = TransactionType.outcome;
+                            });
+                          },
+                        ),
+                      ]),
+                    ),
+                  )
+                : Container(
+                    height: 200,
+                    alignment: Alignment.center,
+                    child: Text('Add categories from settings please!'),
+                  ),
+          ],
+        ));
   }
 }
